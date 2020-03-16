fd030785c6e7327c1ccd949622916847a34469e6
Kris Coppieters <kris@rorohiko.com>
2020-03-17 10:53:00 +1300

    Added error handling. Added optional logging. Re-arranged the functions into alphabetical order.

--- ShowNotes.jsx
+++ ShowNotes.jsx
@@ -1,7 +1,7 @@
 ﻿//
 // ShowNotes.jsx - a script for Adobe InDesign and InCopy 
 //
-// Version 1.02 - 7-March-2020
+// Version 1.0.3 - 17-March-2020
 //
 // by Kris Coppieters 
 // kris@rorohiko.com
@@ -49,20 +49,87 @@ var gGotoFirstNote = true;
 var gZoomPercentage = 250;
 var gShowPanel = true;
 var gMillisecondsToWaitForIdle = 1000;
+var gLogging = false;
 
 // END OF CONFIGURATION
 
 var gNotesByDocumentKey = {};
+var gLogFile = File(Folder.desktop + "/ShowNotes.log");
+
+main();
+
+//--------
+
+function afterOpenHandler(evt) {
+
+    do {
+
+        try {
+
+            if (evt.eventType != Document.AFTER_OPEN) {
+                break;
+            }
+
+            var document = evt.target;
+            if (! (document instanceof Document)) { 
+                break;
+            }
+
+            if (! document.isValid) {
+                break;
+            }
+        
+            var hasNotes = detectNotes(document);
+
+            var handleNote = function() {
+                alert("The 'ShowNotes.jsx' startup script reports:\nThere are Notes in this document.");
+                if (gGotoFirstNote) {
+                    gotoCurrentNote();
+                }
+            };
+            
+            if (hasNotes && gShowAlert) {
+                var idleTask = app.idleTasks.add({
+                    sleep: gMillisecondsToWaitForIdle
+                });    
+          
+                var noteTaskEventListener = idleTask.addEventListener(IdleTask.ON_IDLE, function() {
+                    try {
+                        idleTask.sleep = 0;
+                        handleNote();
+                    }
+                    catch (err) {
+                        logError("afterOpenHandler task listener: throws " + err);
+                    }
+                });
+            }
+
+        }
+        catch (err) {
+            logError("afterOpenHandler: throws " + err);
+        }
+    }
+    while (false);
+}
 
 function cleanupNotes() {
 
     try {
+
         var seenDocs = {};
         var docCount = app.documents.length;
+
         for (var docIdx = 0; docIdx < docCount; docIdx++) {
-            var document = app.documents.item(docIdx);
-            var documentKey = getDocumentKey(document);
-            seenDocs[documentKey] = docIdx;            
+            try {
+                var document = app.documents.item(docIdx);
+                if (document.isValid) {
+                    var documentKey = getDocumentKey(document);
+                    seenDocs[documentKey] = docIdx;
+                }
+            }
+            catch (err) { 
+                logError("cleanupNotes: loop throws " + err);
+            }
         }
 
         var goneDocs = {};
@@ -77,80 +144,8 @@ function cleanupNotes() {
         }
     }
     catch (err) {    
+        logError("cleanupNotes: throws " + err);
     }
-
-}
-
-function gotoCurrentNote() {
-
-    do {
-
-        if (! (app.activeDocument instanceof Document)) {
-            break;
-        }
-
-        var document = app.activeDocument;
-        var documentKey = getDocumentKey(document);
-        if (! (documentKey in gNotesByDocumentKey)) {
-            detectNotes(document);
-        }
-
-        var docEntry = gNotesByDocumentKey[documentKey];
-        if (! docEntry) {
-            break;
-        }
-
-        if 
-        (
-            docEntry.curNoteIdx < 0 
-        || 
-            docEntry.curNoteIdx >= docEntry.noteEntries.length
-        ) {
-            docEntry.curNoteIdx = 0;
-        }
-
-        if (docEntry.curNoteIdx >= docEntry.noteEntries.length) {
-            break;
-        }
-
-        var noteEntry = docEntry.noteEntries[docEntry.curNoteIdx];
-        if (! noteEntry) {
-            break;
-        }
-
-        var story = document.stories.itemByID(noteEntry.storyId);
-        if (! story || ! story.isValid) {
-            break;
-        }
-
-        var note = story.notes.itemByID(noteEntry.noteId);
-        if (! note || ! note.isValid) {
-            break;
-        }
-
-        app.selection = story.characters.item(note.storyOffset.index);  
-        
-        story.characters.item(note.storyOffset.index).showText();
-        
-        app.activeWindow.zoomPercentage = gZoomPercentage;
-        
-        if (gShowPanel) {
-            showPanel("Notes");
-        }
-    }
-    while (false);
-}
-
-function getDocumentKey(document) {
-    var documentKey;
-    if (document.saved) {
-        documentKey = document.fullName.absoluteURI;
-    }
-    else {
-        documentKey = "unsaved:" + document.name;
-    }
-
-    return documentKey;
 }
 
 function detectNotes(document) {
@@ -158,11 +153,16 @@ function detectNotes(document) {
     var hasNotes = false;
 
     do {
+
         try {    
 
             cleanupNotes();
             
             if (! (document instanceof Document)) {
+                break;
+            }
+
+            if (! document.isValid) {
                 break;
             }
 
@@ -180,26 +180,37 @@ function detectNotes(document) {
 
             var storyCount = document.stories.length;
             for (var storyIdx = 0; storyIdx < storyCount; storyIdx++) {
-                var story = document.stories.item(storyIdx);
-                if (story.isValid) {
-                    var storyId = story.id;
-                    var noteCount = story.notes.length;
-                    for (var noteIdx = 0; noteIdx < noteCount; noteIdx++) {
-                        var note = story.notes.item(noteIdx);
-                        if (note.isValid) {
-                            var noteId = note.id;
-                            var noteEntry = {
-                                storyId: storyId,
-                                noteId: noteId
-                            };
-                            hasNotes = true;
-                            docEntry.noteEntries.push(noteEntry);
+                try {
+                    var story = document.stories.item(storyIdx);
+                    if (story.isValid) {
+                        var storyId = story.id;
+                        var noteCount = story.notes.length;
+                        for (var noteIdx = 0; noteIdx < noteCount; noteIdx++) {
+                            try {
+                                var note = story.notes.item(noteIdx);
+                                if (note.isValid) {
+                                    var noteId = note.id;
+                                    var noteEntry = {
+                                        storyId: storyId,
+                                        noteId: noteId
+                                    };
+                                    hasNotes = true;
+                                    docEntry.noteEntries.push(noteEntry);
+                                }
+                            }
+                            catch (err) {
+                                logError("detectNotes: note loop throws " + err);
+                            }
                         }
-                    }
+                    }                    
+                }
+                catch (err) {
+                    logError("detectNotes: story loop throws " + err);
                 }
             }
         }
         catch (err) {
+            logError("detectNotes: throws " + err);
         }
     }
     while (false);
@@ -207,46 +218,152 @@ function detectNotes(document) {
     return hasNotes;
 }
 
-function afterOpenHandler(evt) {
+function getDocumentKey(document) {
+
+    var documentKey = "invalid:";
+
+    do {
+        try {
+
+            if (! (document instanceof Document)) {
+                break;
+            }
+
+            if (! document.isValid) {
+                break;
+            }
+
+            if (document.saved) {
+                documentKey = document.fullName.absoluteURI;
+            }
+            else {
+                documentKey = "unsaved:" + document.name;
+            }
+
+        }
+        catch (err) {
+            logError("getDocumentKey: throws " + err);
+        }
+    }
+    while (false);
+
+    return documentKey;
+}
+
+function gotoCurrentNote() {
 
     do {
 
         try {
 
-            if (evt.eventType != Document.AFTER_OPEN) {
+            var document = app.activeDocument;
+
+            if (! (document instanceof Document)) {
                 break;
             }
 
-            if (! evt.target instanceof Document) { 
+            if (! document.isValid) {
                 break;
             }
 
-            var document = evt.target;
-            var hasNotes = detectNotes(document);
+            var documentKey = getDocumentKey(document);
+            if (! (documentKey in gNotesByDocumentKey)) {
+                detectNotes(document);
+            }
 
-            var handleNote = function() {
-                alert("The 'ShowNotes.jsx' startup script reports:\nThere are Notes in this document.");
-                if (gGotoFirstNote) {
-                    gotoCurrentNote();
-                }
-            };
+            var docEntry = gNotesByDocumentKey[documentKey];
+            if (! docEntry) {
+                break;
+            }
+
+            if 
+            (
+                docEntry.curNoteIdx < 0 
+            || 
+                docEntry.curNoteIdx >= docEntry.noteEntries.length
+            ) {
+                docEntry.curNoteIdx = 0;
+            }
+
+            if (docEntry.curNoteIdx >= docEntry.noteEntries.length) {
+                break;
+            }
+
+            var noteEntry = docEntry.noteEntries[docEntry.curNoteIdx];
+            if (! noteEntry) {
+                break;
+            }
+
+            var story = document.stories.itemByID(noteEntry.storyId);
+            if (! story || ! story.isValid) {
+                break;
+            }
+
+            var note = story.notes.itemByID(noteEntry.noteId);
+            if (! note || ! note.isValid) {
+                break;
+            }
+
+            app.selection = story.characters.item(note.storyOffset.index);  
             
-            if (hasNotes && gShowAlert) {
-              var idleTask = app.idleTasks.add({
-                sleep: gMillisecondsToWaitForIdle
-              });    
-          
-              var noteTaskEventListener = idleTask.addEventListener(IdleTask.ON_IDLE, function() {
-                idleTask.sleep = 0;
-                handleNote();
-              });
+            story.characters.item(note.storyOffset.index).showText();
+            
+            app.activeWindow.zoomPercentage = gZoomPercentage;
+            
+            if (gShowPanel) {
+                showPanel("Notes");
             }
-
         }
-        catch (err) {
+        catch (err) {            
+            logError("gotoCurrentNote: throws " + err);
         }
     }
     while (false);
+}
+
+function main() {
+
+    try {
+
+        //
+        // Clear away any old event listeners so we can reinstall them
+        //
+
+        app.eventListeners.everyItem().remove();
+        app.addEventListener(Document.AFTER_OPEN, afterOpenHandler);
+
+        //
+        // Handle the active document if there is one
+        //
+        if (
+            app.documents.length > 0 
+        && 
+            app.activeDocument 
+        && 
+            app.activeDocument instanceof Document
+        ) {
+            afterOpenHandler({ target: app.activeDocument, eventType: Document.AFTER_OPEN});
+        }
+
+    } 
+    catch (err) {
+        logError("main: throws " + err);
+    }
+}
+
+function logError(errorMsg) {
+
+    try {
+        if (gLogging) {
+            gLogFile.open("a");
+            gLogFile.writeln(errorMsg);
+            gLogFile.close();
+        }        
+    }
+    catch (err) {
+        alert("Critical error: the logError function failed and threw an error: " + err);
+    }
+
 }
 
 function showPanel(panelName) {
@@ -260,29 +377,9 @@ function showPanel(panelName) {
             }
         }
         catch (err) {
+            logError("showPanel: throws " + err);
         }
-    }
-    
-}
-
-try {
-
-    //
-    // Clear away any old event listeners so we can reinstall them
-    //
-
-    app.eventListeners.everyItem().remove();
-    app.addEventListener(Document.AFTER_OPEN, afterOpenHandler);
-
-    //
-    // Handle the active document if there is one
-    //
-    if (app.documents.length > 0 && app.activeDocument && app.activeDocument instanceof Document) {
-        afterOpenHandler({ target: app.activeDocument, eventType: Document.AFTER_OPEN});
-    }
-
-} 
-catch (err) {
+    }    
 }
 
 /*************************************************************
